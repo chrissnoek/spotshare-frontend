d43cc39381f618f0f2a75ec9b395d90f54934ce3
Chris Snoek <chrissnoek@Chriss-MacBook-Pro-2.local>
2021-08-12 18:35:15 +0200

    add formats check to locationlist

--- components/shared/LocationList.jsx
+++ components/shared/LocationList.jsx
@@ -4,64 +4,79 @@ import { useRouter } from "next/router";
 import LocationHashtag from "./LocationHashtag.jsx";
 
 const LocationList = ({ location, active, selectLocation }) => {
-  const router = useRouter();
-  let cardClass = active
-    ? "w-full mb-1 border-2 border-green-500"
-    : "w-full mb-1 border-2 border-white";
+  let featuredPhoto = location.photos
+    .sort((a, b) => b.likes - a.likes)[0];
 
-  const goToLocation = (slug, id) => {
-    let data = [];
-    if (sessionStorage.getItem("visitedLocations")) {
-      data = JSON.parse(sessionStorage.getItem("visitedLocations"));
+  if (featuredPhoto.photo[0].formats) {
+    let imageUrl = '';
+
+    if (featuredPhoto.photo[0].formats.medium) {
+      imageUrl = featuredPhoto.photo[0].formats.medium.url;
+    } else if (featuredPhoto.photo[0].formats.large) {
+      imageUrl = featuredPhoto.photo[0].formats.large.url;
+    } else {
+      imageUrl = featuredPhoto.photo[0].url;
     }
-    if (data.indexOf(id) === -1) {
-      data.push(id);
-    }
-    sessionStorage.setItem("visitedLocations", JSON.stringify(data));
 
-    router.push(`/fotolocatie/${slug}`);
-  };
+    const router = useRouter();
+    let cardClass = active
+      ? "w-full mb-1 border-2 border-green-500"
+      : "w-full mb-1 border-2 border-white";
 
-  return (
-    <div>
-      <div
-        onMouseOver={() => {
-          selectLocation(location.id);
-        }}
-        onMouseOut={() => {
-          selectLocation("");
-        }}
-        className={`relative shadow hover:shadow-lg transition ease-in-out rounded ${cardClass}`}
-      >
+    const goToLocation = (slug, id) => {
+      let data = [];
+      if (sessionStorage.getItem("visitedLocations")) {
+        data = JSON.parse(sessionStorage.getItem("visitedLocations"));
+      }
+      if (data.indexOf(id) === -1) {
+        data.push(id);
+      }
+      sessionStorage.setItem("visitedLocations", JSON.stringify(data));
+
+      router.push(`/fotolocatie/${slug}`);
+    };
+
+    return (
+      <div>
         <div
-          className="flex items-center p-1"
-          onClick={() => {
-            goToLocation(location.slug, location.id);
+          onMouseOver={() => {
+            selectLocation(location.id);
           }}
+          onMouseOut={() => {
+            selectLocation("");
+          }}
+          className={`relative shadow hover:shadow-lg transition ease-in-out rounded ${cardClass}`}
         >
-          <Image
-            className={`rounded block max-w-none w-20 h-16 object-cover`}
-            width={76}
-            height={76}
-            style={{
-              backgroundColor: "grey",
+          <div
+            className="flex items-center p-1"
+            onClick={() => {
+              goToLocation(location.slug, location.id);
             }}
-            src={location.photos
-              .sort((a, b) => b.likes - a.likes)[0]
-              .photo[0].formats.thumbnail.url}
-            alt={`Bekijk locatie ${location.title}`}
-          />
+          >
+            <Image
+              className={`rounded block max-w-none w-20 h-16 object-cover`}
+              width={76}
+              height={76}
+              style={{
+                backgroundColor: "grey",
+              }}
+              src={imageUrl}
+              alt={`Bekijk locatie ${location.title}`}
+            />
 
-          <div className="px-5 py-2">
-            <h3 className="text-black text-lg">{location.title}</h3>
-            {location.location_categories.map((category) => (
-              <LocationHashtag key={category.id} category={category} />
-            ))}
+            <div className="px-5 py-2">
+              <h3 className="text-black text-lg">{location.title}</h3>
+              {location.location_categories.map((category) => (
+                <LocationHashtag key={category.id} category={category} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 
 export default LocationList;
