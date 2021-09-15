343ab47d200368dc4bde37fe5110ecdfe380fe67
Chris Snoek <chrissnoek@Chriss-MacBook-Pro-2.local>
2021-09-15 08:56:06 +0200

    fix save button on location page

--- components/shared/favButton.jsx
+++ components/shared/favButton.jsx
@@ -28,7 +28,7 @@ const FavButton = ({
 
   if(type === 'photo') {
     icon = <FaHeart/>;
-    if(icon) {
+    if(type === "photo") {
       className = `cursor-pointer flex items-center text-2xl ${liked ? 'text-red-500 group-hover:text-red-600' : 'text-gray-500 group-hover:text-red-500'}`
     } else {
       className = `text-white py-2 px-3 rounded-full flex items-center pointer cursor-pointer ${liked ? 'bg-gray-500 hover:bg-gray-600' : 'bg-red-500 hover:bg-red-600'}`
@@ -41,8 +41,8 @@ const FavButton = ({
       className={`group ${className}`}
       onClick={loading ? () => {} : onBtnClick}
     >
-      <div className={`${icon && className} mr-2`}>{loading ? <FaSpinner /> : icon}</div>
-      <div>{!icon && (liked ? 'Verwijderen' : 'Opslaan')}</div>
+      <div className={`${type === "photo" && className} mr-2`}>{loading ? <FaSpinner /> : icon}</div>
+      <div>{type !== 'photo' && (liked ? 'Verwijderen' : 'Opslaan')}</div>
       <div>{icon && count}</div>
     </div>
   );
