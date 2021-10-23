c1b85540fc16fb12aea06b1abb18fc9c0ba475e0
Chris Snoek <chrissnoek@Chriss-MacBook-Pro-2.local>
2021-10-23 11:08:03 +0200

    fix blogposts link

--- components/Dashboard/BlogPosts.jsx
+++ components/Dashboard/BlogPosts.jsx
@@ -22,10 +22,10 @@ const BlogPosts = () => {
             return (
                 <div>
                     <div className="flex justify-center items-center mb-4 cursor-pointer">
-                        <a className="mr-4 rounded flex-none cursor-pointer" href={post.url}>
+                        <a className="mr-4 rounded flex-none cursor-pointer" href={`https://spotshare.nl${post.hero_image}`}>
                             <img className="rounded h-20 w-20 object-cover cursor-pointer" alt={post.title} src={`https://spotshare.nl${post.hero_image}`} />
                         </a>
-                        <a className="text-sm font-bold w-full cursor-pointer break-word" href={post.url}>
+                        <a className="text-sm font-bold w-full cursor-pointer break-word" href={`https://spotshare.nl${post.hero_image}`}>
                             {post.title}
                         </a>
                     </div>
